df4e5d4079a67e7729659486151c5fd4a517d5b5
Yuriy <yuriy@itmint.ca>
2020-08-31 14:13:07 +0300

    add migration Initial_Create

---
+++ Blog/DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace DataAccessLayer.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLayer.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
